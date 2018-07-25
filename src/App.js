001c1f5a68621d995c34c657e238104a881c3947
Bryce Oldham <boldham@intersysconsulting.com>
2018-07-25 10:54:30 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Seal_of_the_United_States_Congress.svg/170px-Seal_of_the_United_States_Congress.svg.png" className="App-logo" alt="logo" />
+          {/* <h1 className="App-title">Congress Data</h1> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
